3be68d1b5a5098c9b1bfa32d51be0db416a50cb5
Houssam Abd El Moniem <31729534+Houssam5@users.noreply.github.com>
2020-05-05 20:20:38 +0200

    editing route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/payment', 'BillingController@paymentMethod');
 Route::post('/','BillingController@savePayment');
 Route::get('/register/submit','customerscontroller@register');
